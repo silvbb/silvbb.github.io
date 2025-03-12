556f93ca35e99303783922e1d0592807034a152d
FiliRon <68350162+silvbb@users.noreply.github.com>
2025-03-12 15:24:11 +0800

    2323

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -1,41 +1,95 @@
-import type { DefaultTheme } from 'vitepress';
+import type { DefaultTheme } from "vitepress";
 
-export const nav: DefaultTheme.Config['nav'] = [
+export const nav: DefaultTheme.Config["nav"] = [
   {
-    text: '我的分类',
+    text: "我的智能体",
     items: [
-      { text: 'Bug万象集', link: '/categories/issues/index', activeMatch: '/categories/issues/' },
-      { text: '"杂碎"逆袭史', link: '/categories/fragments/index', activeMatch: '/categories/fragments/' },
-      { text: '工具四海谈', link: '/categories/tools/index', activeMatch: '/categories/tools/' },
-      { text: '方案春秋志', link: '/categories/solutions/index', activeMatch: '/categories/solutions/' }
+      {
+        text: "JWisdom",
+        link: "https://www.coze.cn/home",
+        activeMatch: "/categories/issues/",
+      },
+      {
+        text: "WiseAgent",
+        link: "https://www.coze.cn/home",
+        activeMatch: "/categories/fragments/",
+      },
+      {
+        text: "MyClaude",
+        link: "https://www.claude.com/",
+        activeMatch: "/categories/tools/",
+      },
+      {
+        text: "Jxflow",
+        link: "https://www.runninghub.cn/",
+        activeMatch: "/categories/solutions/",
+      },
     ],
-    activeMatch: '/categories/'
+    activeMatch: "/categories/",
   },
   {
-    text: '我的小册',
+    text: "我的分类",
     items: [
-      { text: 'Java基础快速入门', link: '/courses/java/index', activeMatch: '/courses/java/' },
-      { text: 'MySQL快速入门', link: '/courses/mysql/index', activeMatch: '/courses/mysql/' },
-      { text: 'MyBatis快速入门', link: '/courses/mybatis/index', activeMatch: '/courses/mybatis/' }
+      {
+        text: "Bug万象集",
+        link: "/categories/issues/index",
+        activeMatch: "/categories/issues/",
+      },
+      {
+        text: '"杂碎"逆袭史',
+        link: "/categories/fragments/index",
+        activeMatch: "/categories/fragments/",
+      },
+      {
+        text: "工具四海谈",
+        link: "/categories/tools/index",
+        activeMatch: "/categories/tools/",
+      },
+      {
+        text: "方案春秋志",
+        link: "/categories/solutions/index",
+        activeMatch: "/categories/solutions/",
+      },
     ],
-    activeMatch: '/courses/'
+    activeMatch: "/categories/",
   },
   {
-    text: '我的标签',
-    link: '/tags',
-    activeMatch: '/tags'
-  },
-  {
-    text: '我的归档',
-    link: '/archives',
-    activeMatch: '/archives'
-  },
-  {
-    text: '关于',
+    text: "我的小册",
     items: [
-      { text: '关于知识库', link: '/about/index', activeMatch: '/about/index' },
-      { text: '关于我', link: '/about/me', activeMatch: '/about/me' }
+      {
+        text: "Java基础快速入门",
+        link: "/courses/java/index",
+        activeMatch: "/courses/java/",
+      },
+      {
+        text: "MySQL快速入门",
+        link: "/courses/mysql/index",
+        activeMatch: "/courses/mysql/",
+      },
+      {
+        text: "MyBatis快速入门",
+        link: "/courses/mybatis/index",
+        activeMatch: "/courses/mybatis/",
+      },
     ],
-    activeMatch: '/about/' // // 当前页面处于匹配路径下时, 对应导航菜单将突出显示
+    activeMatch: "/courses/",
+  },
+  {
+    text: "我的标签",
+    link: "/tags",
+    activeMatch: "/tags",
+  },
+  {
+    text: "我的归档",
+    link: "/archives",
+    activeMatch: "/archives",
+  },
+  {
+    text: "关于",
+    items: [
+      { text: "关于知识库", link: "/about/index", activeMatch: "/about/index" },
+      { text: "关于我", link: "/about/me", activeMatch: "/about/me" },
+    ],
+    activeMatch: "/about/", // // 当前页面处于匹配路径下时, 对应导航菜单将突出显示
   },
 ];
